ef1b5792a0f5c6fe53ac39be1e413e1fc8346207
heebmohammad <52503790+heebmohammad@users.noreply.github.com>
2025-03-23 17:08:53 +0200

    Booking Entity

---
+++ src/main/java/com/att/tdp/popcorn_palace/booking/Booking.java
@@ -0,0 +1,79 @@
+package com.att.tdp.popcorn_palace.booking;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Booking {
+    public static final Integer MAX_SEATS = 9999;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "booking_id")
+    private UUID bookingId;
+
+    @Column(name = "showtime_id")
+    private Long showtimeId;
+
+    @Column(name = "seat_number")
+    private Integer seatNumber;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    // No-argument constructor for JPA
+    public Booking() {
+        // Default constructor
+    }
+
+    // Getters and Setters
+    
+    public UUID getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(UUID bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public Long getShowtimeId() {
+        return showtimeId;
+    }
+
+    public void setShowtimeId(Long showtimeId) {
+        if (showtimeId == null) {
+            throw new IllegalArgumentException("showtimeId is required.");
+        }
+        this.showtimeId = showtimeId;
+    }
+
+    public Integer getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(Integer seatNumber) {
+        if (seatNumber == null || seatNumber < 0) {
+            throw new IllegalArgumentException("Seat number must be a positive number.");
+        }
+        if (seatNumber > MAX_SEATS) {
+            throw new IllegalArgumentException("Exceeding max number of seats: " + MAX_SEATS);
+        }
+        this.seatNumber = seatNumber;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        if (userId == null) {
+            throw new IllegalArgumentException("userId is required.");
+        }
+        this.userId = userId;
+    }
+}
