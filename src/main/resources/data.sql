d0d441e40edf22b63d7efff2ce19ebf62f3d4959
heebmohammad <52503790+heebmohammad@users.noreply.github.com>
2025-03-23 09:46:42 +0200

    Showtime Table

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,14 @@ VALUES
     ('Forrest Gump', 'DRAMA', 142, 8.8, 1994),
     ('Inception', 'SCI_FI', 148, 8.8, 2010),
     ('The Matrix', 'SCI_FI', 136, 8.7, 1999);
+
+DELETE FROM Showtime;
+INSERT INTO Showtime (movie_id, price, theater, start_time, end_time)
+VALUES 
+    ((SELECT id FROM Movie WHERE title = 'The Shawshank Redemption'), 12, 'AMC Theater', '2025-02-14T11:47:46.125405Z', '2025-02-14T14:47:46.125405Z'),
+    ((SELECT id FROM Movie WHERE title = 'The Dark Knight'), 18, 'Regal Cinema', '2025-02-14T12:00:00Z', '2025-02-14T14:30:00Z'),
+    ((SELECT id FROM Movie WHERE title = 'Forrest Gump'), 15, 'Cinemark', '2025-02-15T09:30:00Z', '2025-02-15T12:00:00Z'),
+    ((SELECT id FROM Movie WHERE title = 'Inception'), 14, 'Cineplex', '2025-02-15T13:00:00Z', '2025-02-15T15:30:00Z'),
+    ((SELECT id FROM Movie WHERE title = 'The Matrix'), 16, 'AMC Theater', '2025-02-16T15:00:00Z', '2025-02-16T17:00:00Z'),
+    ((SELECT id FROM Movie WHERE title = 'The Shawshank Redemption'), 12, 'Cinemark', '2025-02-16T18:00:00Z', '2025-02-16T20:30:00Z'),
+    ((SELECT id FROM Movie WHERE title = 'The Dark Knight'), 18, 'Regal Cinema', '2025-02-17T20:00:00Z', '2025-02-17T22:30:00Z');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,9 +1,20 @@
 
 CREATE TABLE IF NOT EXISTS Movie (
-    id SERIAL PRIMARY KEY,           -- BIGSERIAL for auto-generating IDs in PostgreSQL
+    id SERIAL PRIMARY KEY,           -- SERIAL for auto-generating IDs in PostgreSQL
     title VARCHAR(255) NOT NULL UNIQUE, -- Title should be unique
     genre VARCHAR(16) NOT NULL,         -- Genre column
-    duration INT NOT NULL,              -- Duration column
+    duration INT NOT NULL CHECK (duration > 0),              -- Duration column
     rating FLOAT NOT NULL CHECK (rating >= 0 AND rating <= 10),  -- Rating between 0 and 10
     release_year INT NOT NULL            -- Release year column
+);
+
+CREATE TABLE IF NOT EXISTS Showtime (
+    id SERIAL PRIMARY KEY,           -- SERIAL for auto-generating IDs in PostgreSQL
+    movie_id BIGINT NOT NULL,
+    price FLOAT NOT NULL CHECK(price > 0),
+    theater VARCHAR(255) NOT NULL,
+    start_time TIMESTAMPTZ NOT NULL,
+    end_time TIMESTAMPTZ NOT NULL,
+    CHECK (start_time < end_time),  -- Ensure start_time is before end_time
+    FOREIGN KEY (movie_id) REFERENCES Movie(id) ON DELETE CASCADE  -- foreign key constraint
 );
